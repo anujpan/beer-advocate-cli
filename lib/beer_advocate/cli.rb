58e0478d33bb8eabb759cd8ce993d9da168d073f
Anuj Panchal <anujpan1@gmail.com>
2018-11-09 16:20:27 -0500

    Initial env setup

---
+++ lib/beer_advocate/cli.rb
@@ -0,0 +1,7 @@
+class CLI
+  def run
+    puts "project"
+  end
+end
+
+CLI.new.run
---
+++ lib/environment.rb
@@ -0,0 +1,6 @@
+require 'pry'
+require 'open-uri'
+require 'nokogiri'
+
+require_relative "./beer_advocate/version"
+require_relative "./beer_advocate/cli"
