0df31081ae7a9caf3bcd869761c01145fc9ea5c0
Anuj Panchal <anujpan1@gmail.com>
2018-11-12 14:22:42 -0500

    rework show full list functionality

--- lib/beer_advocate/cli.rb
+++ lib/beer_advocate/cli.rb
@@ -2,16 +2,18 @@ class CLI
   def run
     Scraper.scrape_list
 
-    show_list[1...50]
+    show_list(25)
     
     puts "\nHello!\nPresented is a list of Beer Advocates Top 250 beers of all time.\nSelect the placement number of any brew to see more info about it.\nFeel free to exit at any time by saying in 'q'"
 
     options
   end
 
-  def show_list
+  def show_list(amount)
+    # stop each at amount if its designated
     Beer.all.each.with_index(1) do |beer, i|
       puts "#{i}. #{beer.name} by #{beer.brewery}"
+      break if i == amount
     end
   end
 
@@ -22,10 +24,10 @@ class CLI
       puts "\n\n\n\n\nGo drink a beer\n\n\n\n\n"
     else
       if @input == "full"
-        show_list
+        show_list(250)
         puts "\nSelect the placement number of any brew to see more info about it or exit by saying 'q'"
       elsif @input.to_i.between?(1, 250)
-        binding.pry
+        puts "\nSelect another placement number to see more info about it or say 'full' to see the full list again.\nFeel free to exit at any time by saying in 'q'"
       else
         puts "\nInvalid response, please try again.\nTo see the full list type 'full', or exit by saying 'q'"
       end
--- lib/beer_advocate/scraper.rb
+++ lib/beer_advocate/scraper.rb
@@ -2,7 +2,9 @@ class Scraper
   @@html = Nokogiri::HTML(open("https://www.beeradvocate.com/lists/top/"))
   
   def self.scrape_list
-    @@html.css('#ba-content table tr')[2...252].each do |value, i|
+    @@html.css('#ba-content table tr').each_with_index do |value, i|
+      next if i == 0 || i == 1
+      
       name = value.css('td')[1].css('a')[0].text
       brewery = value.css('td')[1].css('a')[1].text
 
@@ -11,6 +13,15 @@ class Scraper
   end
 
   def self.scrape_beer
-
+    # get rating
+    # get brewery location
+    # get style
+    # get abv
+    # get description
+    # get availability
+    # get number of ratings
+    # get how many people want it
+    # get how many people have it
+    # get how many people want to trade
   end
 end
