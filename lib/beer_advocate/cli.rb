1bf18b046c51d0e89dfa9a6f703a77a9b58872da
Anuj Panchal <anujpan1@gmail.com>
2018-11-12 15:11:14 -0500

    Display second level of scraped information

--- lib/beer_advocate/cli.rb
+++ lib/beer_advocate/cli.rb
@@ -27,9 +27,10 @@ class CLI
         show_list(250)
         puts "\nSelect the placement number of any brew to see more info about it or exit by saying 'q'"
       elsif @input.to_i.between?(1, 250)
-        # puts Beer.all[@input.to_i].name
-        # scrape new beer
-        Scraper.scrape_beer(Beer.all[@input.to_i])
+        selection = Beer.all[@input.to_i - 1]
+        Scraper.scrape_beer(selection)
+
+        puts "\n#{selection.name} is brewed in #{selection.location} by #{selection.brewery} and is in the style of a(n) #{selection.style}.\nIt has a rating of #{selection.rating} that is based on #{selection.ratings} votes by beer advocate users.\nIf you are looking to get one #{selection.gots} people have them and #{selection.wants} are in the same boat as you"
 
         puts "\nSelect another placement number to see more info about it or say 'full' to see the full list again.\nFeel free to exit at any time by saying in 'q'"
       else
